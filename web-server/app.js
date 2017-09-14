f07852f95e0d0e6dcd92b1151a28744d14ddfd80
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-15 11:10:03 +1200

    Creating a downloads folder to req a pdf files

--- web-server/app.js
+++ web-server/app.js
@@ -6,6 +6,29 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/users', (req, res) => {
+  let users = [{
+    first_name: 'John',
+    last_name: 'Don',
+    age: 34,
+    gender: 'male'
+  }, {
+    first_name: 'Leo',
+    last_name: 'Messi',
+    age: 30,
+    gender: 'male'
+  }, {
+    first_name: 'Maya',
+    last_name: 'James',
+    age: 20,
+    gender: 'female'
+  }]
+  res.json(users)
+})
+
+app.get('/download', (req, res) => {
+  res.download(path.join(__dirname, '/downloads/CodeSchool_MasteringGithubCourse.pdf'))
+})
 app.listen(3000, () => {
   console.log(`Server started on port 3000...`)
 })
