b6729ee45a1a6e57856830af7418d8aff8db4982
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-14 22:24:28 +1200

    setting a static path with index.html and installing nodemon

--- web-server/app.js
+++ web-server/app.js
@@ -1,19 +1,10 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+// setting a static path
 
-app.get('/', (req, res) => {
-  res.send('New server')
-})
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>')
-})
-
-app.get('/users/:name', (req,res) => {
-  let user = req.params.name
-  res.send('<h1>' + user + '</h1>')
-})
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(3000, () => {
   console.log(`Server started on port 3000...`)
